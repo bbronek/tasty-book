53f26a364e46541c6a3535ad6c689f4b21297b26
bbronek <barbro214@gmail.com>
2021-08-27 17:05:10 +0200

    Fix bug in infinite scroll

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,12 +14,12 @@ class RecipesController < ApplicationController
     @pagy, @recipes = pagy(recipes, items: per_page)
 =======
     recipes = Recipe.sort_by_kind_and_order(@sort_kind, @sort_order)
-    @pagy, @recipes = pagy(Recipe.all, items: per_page)
+    @pagy, @recipes = pagy(recipes, items: per_page)
 
-    respond_to do |format| 
+    respond_to do |format|
       format.html
       format.json {
-        render json: { entries: render_to_string(partial: "recipes/recipes", formats: [:html]), pagination: view_context.pagy_nav(@pagy) }
+        render json: {entries: render_to_string(partial: "recipes/recipes", formats: [:html]), pagination: view_context.pagy_nav(@pagy)}
       }
     end
 >>>>>>> 0c6c81f (Add infinite scroll)
