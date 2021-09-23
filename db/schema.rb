d11f2527e236447ec3efd1d639703237545e1792
bbronek <barbro214@gmail.com>
2021-09-23 20:17:03 +0200

    Update Readme.md

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_185423) do
+ActiveRecord::Schema.define(version: 2021_08_31_091417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,9 +128,9 @@ ActiveRecord::Schema.define(version: 2021_08_25_185423) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "difficulty", default: 0, null: false
-    t.integer "time_in_minutes_needed", null: false
-    t.integer "status", default: 0, null: false
+    t.integer "time_in_minutes_needed", default: 0, null: false
     t.integer "layout", default: 0, null: false
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
